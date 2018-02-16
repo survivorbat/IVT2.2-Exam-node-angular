67851ca7d117002d8615dc96ebc9528a7c815880
AlexBrooklyn <djbatcat@gmail.com>
2018-02-16 21:20:56 +0100

    Added temponary functions and properties to login prompt

--- client/src/app/components/forms/login/login.component.ts
+++ client/src/app/components/forms/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import User from '../../../domain/User';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  user: User;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submit(){
+    
   }
 
 }
