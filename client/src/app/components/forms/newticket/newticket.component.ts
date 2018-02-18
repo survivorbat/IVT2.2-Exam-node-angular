42d9ee2ad402cc04a1eb23d45876416487a038bf
AlexBrooklyn <djbatcat@gmail.com>
2018-02-18 11:27:22 +0100

    Added ticket select

--- client/src/app/components/forms/newticket/newticket.component.ts
+++ client/src/app/components/forms/newticket/newticket.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import Ticket from '../../../domain/Ticket';
+import Showing from '../../../domain/Showing';
+import { ShowingsService } from '../../../services/showingservice.service';
 
 @Component({
   selector: 'app-newticket',
@@ -7,17 +9,32 @@ import Ticket from '../../../domain/Ticket';
   styleUrls: ['./newticket.component.scss']
 })
 export class NewticketComponent implements OnInit {
-  newTicket: Ticket;
+  newTicket: Ticket[];
   loading: boolean;
-  
-  constructor() { }
+  _showing: Showing;
+
+  @Input() set showing(showing: Showing){
+    this._showing = showing;
+  }
+  constructor(private showingservice: ShowingsService) { this._showing=new Showing()}
 
   ngOnInit() {
+    
   }
   isLoggedIn(){
     if(window.localStorage.getItem('loggedin')==='true'){
       return true;
     }
     return false;
+  }
+  getArrayFromNumber(num: Number): Number[]{
+    let rs: Number[] = [];
+    for(let i=0;i<num;i++){
+      rs.push(i);
+    }
+    return rs;
+  }
+  submit(){
+
   }
 }
